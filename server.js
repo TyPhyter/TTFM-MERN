6003c8fdbb972c7c2b03b4a220ddab531159e108
TyPhyter <tylerjenkins08@gmail.com>
2018-07-02 19:36:29 -0500

    merge commit

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const PORT = 3001;
-const mongoose = require('mongoose');
 
+const mongoose = require('mongoose');
 const db = require('./models');
+mongoose.connect("mongodb://localhost/ttfm", () => {
+    console.log('Connected to db');
+});
+
 
 app.listen(PORT, ()=> {
-    console.log('Listening on ', PORT);
-})
+    console.log('Listening on', PORT);
+});
