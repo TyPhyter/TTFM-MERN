8e889e1edb879d1e18fd363a0e9c8b540a971faa
tmosley03 <tlm3dev@gmail.com>
2018-07-09 11:28:41 -0500

    added project and test controllers

---
+++ controllers/project-controller.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../models");
+const app = require("mongoose");
+
+
+//add Project
+router.post('/projects', (req, res) => {
+    let project = {
+        title: req.body.title,
+        body: req.body.body,
+        repoUrl: req.body.repoUrl,
+        hostedUrl: req.body.hostedUrl,
+               UserId: req.body.UserId,
+        authorAvatarUrl: req.body.authorAvatarUrl
+    }
+
+
+    db.Project.create(project)
+        .then((project) => {
+            res.send(project);
+        });
+   // res.render('index', {});
+});
+
+
+//==================================
+
+//get Projects, all or by project id
+router.get('/projects/:id?', (req, res) => {
+    if (req.params.id) {
+        let id = req.params.id;
+        db.Project.findOne({
+            where: { Userid: id },
+            include: [{ all: true }]
+        })
+            .then((project) => {
+                console.log(project.dataValues);
+                res.render('projectDetail', project.dataValues);
+            });
+    } else {
+        db.Project.findAll({ order: [['updatedAt', 'DESC']] })
+            .then((projects) => {
+                console.log(projects);
+                res.render('allProjects', projects);
+            });
+    }
+});
+
+//======================================
+
+//get Projects by user id
+router.get('/projects/user/:id', (req, res) => {
+
+    let id = req.params.id;
+    db.Project.findAll({ where: { UserId: id }, order: [['updatedAt', 'DESC']] })
+        .then((projects) => {
+            res.send(projects);
+        });
+
+});
+
+//======================================
+
+//get page for posting Projects by user id
+router.get('/projects/post/:id', (req, res) => {
+    let userid = req.params.id;
+    res.render('projectMaker', { userid });
+
+});
+
+//=============
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const app = express();
 const PORT = 3001;
+const bodyParser = require("body-parser");
+//body parser needs to be at the top in order to work
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+//const router = require("./routes/router");
+//const userRouter = require("./controllers/user-controller");
+//const projectRouter = require("./controllers/project-controller");
+const testRouter = require("./controllers/test-controller");
+//app.use("/", router);
+//app.use("/", userRouter);
+// app.use("/", projectRouter);
+app.use("/", testRouter);
+
+
+
 
 const mongoose = require('mongoose');
 const db = require('./models');
-mongoose.connect("mongodb://localhost/ttfm", () => {
+mongoose.connect("mongodb://localhost:27017/ttfn", () => {
     console.log('Connected to db');
 });
 
