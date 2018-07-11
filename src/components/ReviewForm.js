b224b4c6c79d3d2ec330a42dae2f0cc6dc463907
Grace Ho <rainczech@gmail.com>
2018-07-11 17:24:02 -0500

    j/k cleaned up review page for real

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -4,98 +4,54 @@ export default class ReviewForm extends Component {
     render () {
         return (
             <div className="container app-wrapper">
-            <div className="row">
-                <div className="card col s5 m4">
-                    <div className="card-content">
-                        <p>New to TestThisFor.Me? Sign up! It takes only a few seconds to change your coding life!</p>
-                    </div>
-                    <div className="card-tabs">
-                        <ul className="tabs tabs-fixed-width">
-                            <li className="tab">
-                                <a href="#test5">Sign In</a>
-                            </li>
-                            <li className="tab">
-                                <a href="#test4">Sign Up</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="card-content grey lighten-4">
-                        <div id="test4">
-                            <div className="row">
-                                <div className="card col s12 m12">
-                                    <div className="row">
-                                        <form className="col s12">
-                                            <div className="row">
-                                                <div className="input-field col s12">
-                                                    <input id="su_user_name" type="text" className="validate"/>
-                                                    <label for="su_user_name">
-                                                        User Name
-                                                    </label>
-                                                </div>
-                                                <div className="input-field col s12">
-                                                        <input id="su_email" type="email" className="validate"/>
-                                                        <label for="su_email">Email</label>
-                                                </div>
-                                                <div className="input-field col s12">
-                                                    <input id="su_password" type="password" className="validate"/>
-                                                    <label for="su_password">Password</label>
-                                                </div>
-                                                
-                                            </div>
-                                            <button id="su_submit" className="btn waves-effect waves-light btn-small" type="submit" name="action">
-                                                Submit
-                                            </button>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div id="test5">
-                            <div className="row">
-                                <div className="card col s12 m12">
-                                    <div className="row">
-                                        <h3>Sign in with either your GitHub account or username!</h3>
-                                    </div>
-                                    <div className="row">
-                                        <a href="" className="center-align">
-                                            <img id="gitLogo" src="./img/if_mark-github_298822.png"/>
-                                            <div className="black-text">Sign In</div>
-                                        </a>
-                                        <br/>
-                                        <div className="center-align">
-                                            -or-
-                                        </div>
-                                    </div>
-                                    <div className="row">
-                                        <form className="col s12">
-                                            <div className="row">
-                                                <div className="input-field col s12">
-                                                    <input id="li_user_name" type="text" className="validate"/>
-                                                    <label for="user_name">Email</label>
-                                                </div>
-                                                <div className="input-field col s12">
-                                                    <input id="li_password" type="password" className="validate"/>
-                                                    <label for="password">
-                                                        Password
-                                                    </label>
-                                                </div>
-                                            </div>
-                                            <button id="li_submit" className="btn waves-effect waves-light btn-small" type="submit" name="action">
-                                                Submit
-                                            </button>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="card col s6 m7 offset-m1 offset-s1" id="content">
+        <h2 id="projectName">Project Title</h2>
+        <div className="row">
+            <div className="col s12">
+                <div className="card">
                     <div className="row">
-                        <img id="instructions" src="./img/LandingPg-Animated.gif"/>
+                        <div className="card-header">
+                            <h2 className="feedback-heading">How Did It Go?</h2>
+                        </div>
+                    </div>
+                    <div className="card-content">
+                        <div className="row">
+                            <form className="col s12">
+                                <div className="row">
+                                    <ul>
+                                        <li className="col s4">
+                                            <label>
+                                                <input className="with-gap" name="group3" type="radio" value="5" />
+                                                <span>Everything worked!</span>
+                                            </label>
+                                        </li>
+                                        <li className="col s4">
+                                            <label>
+                                                <input className="with-gap" name="group3" type="radio" value="3" />
+                                                <span>Looks good, but has a few issues.</span>
+                                            </label>
+                                        </li>
+                                        <li className="col s4">
+                                            <label>
+                                                <input className="with-gap" name="group3" type="radio" value="1" />
+                                                <span>So broke, it eats sleep for dinner.</span>
+                                            </label>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className="row">
+                                    <div className="input-field col s12">
+                                        <textarea id="textarea1" className="materialize-textarea"></textarea>
+                                        <label for="textarea1">Specific Feedback and Comments</label>
+                                    </div>
+                                </div>
+                                <button className="btn waves-effect waves-light btn-small" type="submit" name="action">Submit
+                                </button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
+        </div>
         </div>
         )
     }
