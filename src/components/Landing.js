6e0eb564a5f3489d3274188801c4eb1d62091394
Grace Ho <rainczech@gmail.com>
2018-07-17 15:37:25 -0500

    landing page sign in methods

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -20,65 +20,65 @@ export default class Landing extends Component {
     }
 
     signUpSubmit = event => {
-            event.preventDefault();
-            let redirectUri = "";
-            let localUri = 'http://localhost:8080/users/';
-            let productionUri = 'http://www.testthisfor.me/users/';
-            let su_email = document.querySelector('#su_email').value.trim();
-            let su_password = document.querySelector('#su_password').value.trim();
-            let displayName = document.querySelector('#su_user_name').value.trim();
-            fetch(productionUri, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    email: su_email,
-                    pass: su_password,
-                    displayName: displayName
-                })
+        event.preventDefault();
+        let redirectUri = "";
+        let localUri = 'http://localhost:8080/users/';
+        let productionUri = 'http://www.testthisfor.me/users/';
+        let su_email = document.querySelector('#su_email').value.trim();
+        let su_password = document.querySelector('#su_password').value.trim();
+        let displayName = document.querySelector('#su_user_name').value.trim();
+        fetch(productionUri, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: su_email,
+                pass: su_password,
+                displayName: displayName
             })
-            .then((response) => {
-                // maybe save token here
-                console.log(response);
-                redirectUri = response.url;
-                // return response.json();
-                window.location.href = redirectUri;
-            })
-            .then((json) => {
-                console.log(json);
-            });
-        };
+        })
+        .then((response) => {
+            // maybe save token here
+            console.log(response);
+            redirectUri = response.url;
+            // return response.json();
+            window.location.href = redirectUri;
+        })
+        .then((json) => {
+            console.log(json);
+        });
+    };
         
-          signInSubmit = event => {
-              event.preventDefault();
-              let redirectUri = "";
-              let localUri = 'http://localhost:8080/users/login';
-              let productionUri = 'http://www.testthisfor.me/users/login';
-              let li_email = document.querySelector('#li_user_name').value.trim();
-              let li_password = document.querySelector('#li_password').value.trim();
-                fetch(productionUri, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    email: li_email,
-                    pass: li_password
-                }),
-                // redirect: 'follow'
-            })
-                .then((response) => {
-                //maybe save token here
-                console.log(response);
-                redirectUri = response.url;
-                // return response.json();
-                window.location.href = redirectUri;
-            })
-                .then((json) => {
-                console.log(json);
-            });
-            };
+    signInSubmit = event => {
+        event.preventDefault();
+        let redirectUri = "";
+        let localUri = 'http://localhost:8080/users/login';
+        let productionUri = 'http://www.testthisfor.me/users/login';
+        let li_email = document.querySelector('#li_user_name').value.trim();
+        let li_password = document.querySelector('#li_password').value.trim();
+        fetch(productionUri, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: li_email,
+            pass: li_password
+        }),
+        // redirect: 'follow'
+    })
+        .then((response) => {
+        //maybe save token here
+        console.log(response);
+        redirectUri = response.url;
+        // return response.json();
+        window.location.href = redirectUri;
+    })
+        .then((json) => {
+        console.log(json);
+    });
+    };
 
 
     githubClickHandler = (evt) => {
